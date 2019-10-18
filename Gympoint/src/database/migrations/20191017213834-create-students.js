da10213f419f04f1da6c40693787a5e4f355539e
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2019-10-18 00:10:58 -0300

    primeira parte back-end

--- Gympoint/src/database/migrations/20191017213834-create-students.js
+++ Gympoint/src/database/migrations/20191017213834-create-students.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', {
+    return queryInterface.createTable('students', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -16,9 +16,15 @@ module.exports = {
         allowNull: false,
         unique: true,
       },
-      password_hash: {
-        type: Sequelize.STRING,
+      age: {
+        type: Sequelize.INTEGER,
         allowNull: false,
+      },
+      student_weight: {
+        type: Sequelize.DOUBLE,
+      },
+      student_height: {
+        type: Sequelize.DOUBLE,
       },
       created_at: {
         type: Sequelize.DATE,
@@ -32,6 +38,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('users');
+    return queryInterface.dropTable('students');
   },
 };
--- Gympoint/src/routes.js
+++ Gympoint/src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -10,5 +11,7 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.post('/users', UserController.store);
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
 
 export default routes;

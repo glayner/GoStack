ee1278172f5d49db3c1643832fcd6a1b20865f14
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2019-10-21 16:14:37 -0300

    final da segunda parte back-end

--- modulo02/src/app/controllers/AppointmentController.js
+++ modulo02/src/app/controllers/AppointmentController.js
@@ -109,6 +109,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -133,7 +138,14 @@ class AppointmentController {
     Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);

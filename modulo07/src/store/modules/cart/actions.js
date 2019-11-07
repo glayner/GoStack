445021d51416eaf0238aea05c5cac99e1a23dee0
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2019-11-06 23:01:52 -0300

    modulo07 estoque na alteração

--- modulo07/src/store/modules/cart/actions.js
+++ modulo07/src/store/modules/cart/actions.js
@@ -1,6 +1,12 @@
-export function addToCart(product) {
+export function addToCartRequest(id) {
   return {
-    type: '@cart/ADD',
+    type: '@cart/ADD_REQUEST',
+    id,
+  };
+}
+export function addToCartSuccess(product) {
+  return {
+    type: '@cart/ADD_SUCCESS',
     product,
   };
 }
@@ -12,9 +18,16 @@ export function removeFromCart(id) {
   };
 }
 
-export function updateAmount(id, amount) {
+export function updateAmountRequest(id, amount) {
   return {
-    type: '@cart/UPDATE_AMOUNT',
+    type: '@cart/UPDATE_AMOUNT_REQUEST',
+    id,
+    amount,
+  };
+}
+export function updateAmountSuccess(id, amount) {
+  return {
+    type: '@cart/UPDATE_AMOUNT_SUCCESS',
     id,
     amount,
   };

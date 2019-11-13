8a5c3e50142d5520285f70643b12472dfd34cd88
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2019-11-12 22:51:07 -0300

    armazenando perfil persistindo autenticacao

--- gobarberweb/src/store/index.js
+++ gobarberweb/src/store/index.js
@@ -1,5 +1,8 @@
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+
 import createStore from './createStore';
+import presistReducers from './presistReducers';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -12,8 +15,9 @@ const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleware];
 
-const store = createStore(rootReducer, middlewares);
+const store = createStore(presistReducers(rootReducer), middlewares);
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };

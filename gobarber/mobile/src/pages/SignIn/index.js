5709e3d5fa335074e1dcd0077d4b7e8696c2e547
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2019-11-30 00:05:31 -0300

    structure and reactotron

---
+++ gobarber/mobile/src/pages/SignIn/index.js
@@ -0,0 +1,61 @@
+import React, {useRef} from 'react';
+import PropTypes from 'prop-types';
+import {Image} from 'react-native';
+
+import logo from '~/assets/logo.png';
+
+import BackGround from '~/components/Background';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignIn({navigation}) {
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
+  return (
+    <BackGround>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
+    </BackGround>
+  );
+}
+
+SignIn.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};

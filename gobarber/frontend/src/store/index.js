b8b8e261e5079721d9946109907033b536f9dbca
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2019-12-02 19:47:40 -0300

    fix file name

--- gobarber/frontend/src/store/index.js
+++ gobarber/frontend/src/store/index.js
@@ -2,7 +2,7 @@ import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
 
 import createStore from './createStore';
-import presistReducers from './presistReducers';
+import persistReducers from './persistReducers';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -15,7 +15,7 @@ const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleware];
 
-const store = createStore(presistReducers(rootReducer), middlewares);
+const store = createStore(persistReducers(rootReducer), middlewares);
 const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);

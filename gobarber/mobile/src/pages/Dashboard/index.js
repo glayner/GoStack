d4c5f3731eabad0a870d19d6b519a1dabd05bea3
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2019-12-03 16:53:42 -0300

    style routes list appointment

--- gobarber/mobile/src/pages/Dashboard/index.js
+++ gobarber/mobile/src/pages/Dashboard/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import BackGround from '~/components/Background';
+import Appointment from '~/components/Appointment';
+
+import {Container, Title, List} from './styles';
+
+const data = [1, 2, 3, 4, 5];
 
 export default function Dashboard() {
-  return <View />;
+  return (
+    <BackGround>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({item}) => <Appointment data={item} />}
+        />
+      </Container>
+    </BackGround>
+  );
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamento',
+  tabBarIcon: ({tintColor}) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};

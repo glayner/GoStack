21f98a348436634cc3ebb13c3ce4b15d5ebf953e
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2019-12-04 18:32:41 -0300

    fix order index appointment

--- gobarber/backend/src/app/controllers/AppointmentController.js
+++ gobarber/backend/src/app/controllers/AppointmentController.js
@@ -83,7 +83,7 @@ class AppointmentController {
     const { page = 1 } = req.query;
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
-      order: ['date'],
+      order: [['date', 'DESC']],
       attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,

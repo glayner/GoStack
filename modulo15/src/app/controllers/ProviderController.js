e6f899cc38479cbfaff0096c81f95ae7c332d630
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2019-12-30 22:49:01 -0300

    Cache structure and provider

--- modulo15/src/app/controllers/ProviderController.js
+++ modulo15/src/app/controllers/ProviderController.js
@@ -1,8 +1,15 @@
 import User from '../models/User';
 import File from '../models/File';
 
+import Cache from '../../lib/Cache';
+
 class ProviderController {
   async index(req, res) {
+    const cached = await Cache.get('providers');
+    if (cached) {
+      return res.json(cached);
+    }
+
     const providers = await User.findAll({
       where: { provider: true },
       attributes: ['id', 'name', 'email', 'avatar_id'],
@@ -14,7 +21,9 @@ class ProviderController {
         },
       ],
     });
-    res.json(providers);
+    await Cache.set('providers', providers);
+
+    return res.json(providers);
   }
 }
 
